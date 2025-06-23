bbba2dfab5fb32c29f6961e0e59c5e486aef75df
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-06-23 23:10:00 +0300

    fixes to PEP8

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,5 @@
 def get_mask_card_number(card_number: int) -> str:
-    """Функция для маскировки номера банковской карты """
+    """Функция для маскировки номера банковской карты"""
     card_str = str(card_number).replace(" ", "")
     if len(card_str) != 16:
         raise ValueError
@@ -7,7 +7,7 @@ def get_mask_card_number(card_number: int) -> str:
 
 
 def get_mask_account(account_number: int) -> str:
-    """ Функция маскировки номера банковского счета """
+    """Функция маскировки номера банковского счета"""
     account_number_str = str(account_number).replace(" ", "")
     if not account_number_str.isdigit():
         raise ValueError
@@ -15,14 +15,15 @@ def get_mask_account(account_number: int) -> str:
         raise ValueError
     return f"**{account_number_str[-4:]}"
 
-def actions_user():
+
+def actions_user() -> None:
     while True:
         try:
             user_number_card = int(input("Введите номер карты: "))
             mask_card_number = get_mask_card_number(user_number_card)
             print(f"Номер карты: {mask_card_number}")
             break  # Выход из цикла, если карта введена верно
-        except ValueError as e:
+        except ValueError:
             print("Номер карты должен состоять из 16 символов")
 
     while True:
@@ -31,6 +32,5 @@ def actions_user():
             mask_account_number = get_mask_account(user_account_number)
             print(f"Номер счёта: {mask_account_number}")
             break
-        except ValueError as e:
+        except ValueError:
             print("Номер счета должен состоять только из цифр, не менее 4!")
-
--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,25 @@
 from masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(str_account):
+
+def mask_account_card(str_account: str) -> str:
     """Функция обрабатывает информацию как о картах, так и о счетах"""
-    account_details = str_account.split(' ')
+    account_details = str_account.split(" ")
     for detail in account_details:
         if detail.lower().startswith("счет"):
-            digit_part = next((d for d in account_details if d.isdigit()), None)
-            return f"Счет {get_mask_account(digit_part)}"
+            digit_part = next((d for d in account_details if d.isdigit()))
+            return f"Счет {get_mask_account(int(digit_part))}"
     for detail in account_details:
         if detail.isdigit():
-            card_name = ' '.join([part for part in account_details if not part.isdigit()])
-            return f"{card_name}  {get_mask_card_number(detail)}"
+            card_name = " ".join([part for part in account_details if not part.isdigit()])
+            return f"{card_name}  {get_mask_card_number(int(detail))}"
+    return "Ошибка: неверный формат данных"
+
 
 def get_date(date_str: str) -> str:
     """Преобразует дату из формата 'ГГГГ-ММ-ДДT...' в 'ДД.ММ.ГГГГ'."""
-    date_part = date_str.split('T')[0]
-    year, month, day = date_part.split('-')
+    date_part = date_str.split("T")[0]
+    year, month, day = date_part.split("-")
     return f"{day}.{month}.{year}"
-
 
 
 print(mask_account_card("Maestro Classic 1596837868705199"))
