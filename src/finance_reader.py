894801fe828d58322e82a2be804837df345f3286
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-08-06 22:19:07 +0300

    add finance_reader

---
+++ src/finance_reader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def reader_finance_from_csv(filepath):
+    """Преобразуем файл из формата CSV в словарь"""
+    dataframe = pd.read_csv(filepath)
+    return dataframe.to_dict("records")
+
+
+def reader_finance_from_excel(filepath):
+    """Преобразуем файл из формата EXCEL в словарь"""
+    dataframe = pd.read_excel(filepath, engine="openpyxl")
+    return dataframe.to_dict("records")
--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,12 @@ import json
 import logging
 from logging import DEBUG
 from typing import Any, Dict, List
+
 from src.config import LOGS_DIR
 
 logger = logging.getLogger("utils")
 logger.setLevel(DEBUG)
-file_handler = logging.FileHandler(
-     LOGS_DIR / 'utils.log', encoding="utf-8", mode="w"
-)
+file_handler = logging.FileHandler(LOGS_DIR / "utils.log", encoding="utf-8", mode="w")
 file_formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -33,6 +32,3 @@ def get_financial_transactions(filename: str) -> List[Dict[str, Any]]:
     except FileNotFoundError:
         logger.error("Файл не найден")
         return []
-
-
-
